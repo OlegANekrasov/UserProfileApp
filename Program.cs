cb54a9e7491efc148b3d024a90c1636e27d4d109
Oleg Nekrasov <79853886592@yandex.ru>
2022-03-23 18:49:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserProfileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату рождения: ");
+            var birthdate = Console.ReadLine();
+
+            Console.Write($"Ваше имя: {name}, возраст: {age}, дата рождения: {birthdate}");
+            Console.ReadLine();
+        }
+    }
+}
